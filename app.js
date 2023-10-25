80f01dc36d149b90209be5a0dc9bab76c49b38dc
Gabriel Roldan <roldanbiel@gmail.com>
2023-10-25 14:47:38 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,188 @@
+
+var express = require('express');
+var mysql = require('mysql2');
+var cors = require('cors')
+var app = express();
+app.use(express.json())
+app.use(cors())
+
+const users =  [ {"user": "gabriel"},  {"user": "marcos"} ]
+
+app.get('/', function(req, res) {
+    res.send("Pag Inicial")
+})
+
+app.get('/users',function(req,res) {
+    res.send(users)
+});
+
+app.get('/user/:id', function(req, res) {
+    res.send(users[req.params.id])
+})
+
+app.post('/user', function(req, res, next) {
+    const user = req.body.user
+    const data = { 
+        user: user
+    }
+
+    var usuarioExistente = false;
+
+    for (let i = 0; i < users.length; i++) {
+        // console.log(users[i]);
+        // console.log(data);
+        if (users[i].user === data.user) {
+            usuarioExistente = true;
+            break
+        }
+    }
+    
+    console.log("teste");
+    if (usuarioExistente) {
+        res.status(401)
+        res.send("Usuário já existe")
+        // res.end()
+        next()
+    } else {
+        res.send(data)
+    }
+});
+
+app.get('/contato', function(req, res) {
+    res.send('Pag contato')
+})
+
+app.post('/contato', function(req,res) {
+    console.log(req.body)
+    const nome = req.body.nome
+    const email = req.body.email
+    const mensagem = req.body.mensagem
+    const data = {
+        nome: nome,
+        email: email,
+        mensagem: mensagem
+    }
+    
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "i$>E8]>0&zDOG5c",
+        database: "node_test"
+    });
+    
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Conectado");
+
+        con.query(`INSERT INTO contato (nome, email, mensagem) VALUES ('${data.nome}', '${data.email}', '${data.mensagem}')`)
+            if (err) throw err;
+            console.log("Adicionado");
+    });
+
+    res.send(data);
+});
+
+// retorna json contatos
+app.get('/contatos', function(req, res) {
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "i$>E8]>0&zDOG5c",
+        database: "node_test"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Conectado");
+
+        const query = "SELECT * FROM contato"
+        con.query(query, (err, result) => {
+            if (err) throw err;
+
+            // console.log(result);
+            res.send(result);
+        })
+    })
+})
+
+// retorna usuario do email
+app.get('/contato/:email', function(req, res) {
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "i$>E8]>0&zDOG5c",
+        database: "node_test"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Conectado");
+
+        const query = `SELECT * FROM contato WHERE email='${req.params.email}';`
+        con.query(query, (err, result) => {
+            if (err) throw err;
+
+            console.log(result);
+            res.send(result)
+        })
+    })
+})
+
+// update
+app.put('/contatos/:email', (req, res) => {
+
+    const email = req.body.email
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "i$>E8]>0&zDOG5c",
+        database: "node_test"
+    });
+
+    con.connect( (err) => {
+        if (err) throw err;
+        console.log("Conectado");
+
+        const query = `UPDATE contato SET email='${req.params.email}' WHERE email='${email}';`;
+        console.log(query);
+        con.query(query, (err, result) => {
+            if (err) throw err;
+            console.log("Update Feito");
+            console.log(result);
+            res.send(result);
+        })
+    } )
+})
+
+// delete
+app.delete('/contatos', (req, res) => {
+
+    const email = req.body.email;
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "i$>E8]>0&zDOG5c",
+        database: "node_test"
+    });
+
+    con.connect( (err) => {
+        if (err) throw err;
+        console.log("Conectado")
+
+        const query = `DELETE FROM contato WHERE email='${email}';`
+        con.query(query, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            console.log("Delete Feito")
+            res.send(result);
+        })
+    })
+})
+
+app.listen(8080, function() {
+    console.log("Servidor ativo na porta 8080");
+});
---
+++ script.js
@@ -0,0 +1,29 @@
+
+var button = document.getElementById('button');
+
+button.addEventListener('click', function enviar() {
+    var nome = document.getElementById('nome');
+    var email = document.getElementById('email');
+    var mensagem = document.getElementById('msg');
+
+    if (nome.value != "" && email.value != "") {
+        alert('Enviado com sucesso!');
+    }
+
+    let data = {
+        nome: nome.value,
+        email: email.value,
+        mensagem: mensagem.value
+    }
+    
+    fetch('http://localhost:8080/contato', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json() )
+    .then(response => console.log(json))
+    .catch(err => console.log(err))
+});
+
+var pageTabela;
